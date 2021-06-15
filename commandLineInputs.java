36a73985c280afdb99f884d57af310e39d42c3ce
Hemanth Soma <hemusoma123@gmail.com>
2021-06-15 20:36:22 +0530

    Day02

---
+++ commandLineInputs.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+public class commandLineInputs
+{
+  public static void main(String[] args) {
+    for(int i=0;i<args.length;i++){
+      System.out.println(args[i]);
+    }
+  }
+}
