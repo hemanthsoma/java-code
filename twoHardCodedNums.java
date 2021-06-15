5dc7ad27adafb86a2a6a5efeeffdaae0d430f782
Hemanth Soma <hemusoma123@gmail.com>
2021-06-15 08:38:49 +0530

    Hard Coded Numbers

---
+++ twoHardCodedNums.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class twoHardCodedNums
+{
+  public static void main(String[] args) {
+    int a = 98765,b = 12345;
+    System.out.println("Addtion : "+(a+b));
+    System.out.println("Subtraction : "+(a-b));
+    System.out.println("Division :"+(a/b));
+  }
+}
